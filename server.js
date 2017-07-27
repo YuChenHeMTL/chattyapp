20ac3e6d0d5f2c3b497b50629b319f71fa02d2c0
YuChenHeMTL <yuchen7568@gmail.com>
2017-07-27 20:21:20 +0000

    try 9

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ new WebpackDevServer(webpack(config), {
       ignored: /node_modules/
     }
   })
-  .listen(3000, '0.0.0.0', function (err, result) {
+  .listen(process.env.PORT || 8080, '0.0.0.0', function (err, result) {
     if (err) {
       console.log(err);
     }
