20b9ec40c9e9ebeca3f1eca5cba50ab293021c0f
YuChenHeMTL <yuchen7568@gmail.com>
2017-07-27 21:09:51 +0000

    final commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ new WebpackDevServer(webpack(config), {
       ignored: /node_modules/
     }
   })
-  .listen(process.env.PORT || 3000, '0.0.0.0', function (err, result) {
+  .listen(3000, '0.0.0.0', function (err, result) {
     if (err) {
       console.log(err);
     }
